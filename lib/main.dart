b2be750fcab86753825372ad64ea6473cf20cfef
Tavo <tsoto111@gmail.com>
2020-02-27 00:42:25 -0600

    Hooking up sound to button using audioplayers package

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,76 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(filenameIndex) {
+    final player = AudioCache();
+    player.play("note$filenameIndex.wav");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              child: Text("Click Me!"),
-              onPressed: () {
-                final player = AudioCache();
-                player.play("note1.wav");
-              },
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: RawMaterialButton(
+                fillColor: Colors.red,
+                onPressed: () {
+                  playSound(1);
+                },
+              ),
             ),
-          ),
+            Expanded(
+              child: RawMaterialButton(
+                fillColor: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+              ),
+            ),
+            Expanded(
+              child: RawMaterialButton(
+                fillColor: Colors.yellow,
+                onPressed: () {
+                  playSound(3);
+                },
+              ),
+            ),
+            Expanded(
+              child: RawMaterialButton(
+                fillColor: Colors.green,
+                onPressed: () {
+                  playSound(4);
+                },
+              ),
+            ),
+            Expanded(
+              child: RawMaterialButton(
+                fillColor: Colors.green.shade900,
+                onPressed: () {
+                  playSound(5);
+                },
+              ),
+            ),
+            Expanded(
+              child: RawMaterialButton(
+                fillColor: Colors.blue,
+                onPressed: () {
+                  playSound(6);
+                },
+              ),
+            ),
+            Expanded(
+              child: RawMaterialButton(
+                fillColor: Colors.purple,
+                onPressed: () {
+                  playSound(7);
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
