0cbb43a75d6ab36ee3fbf5362bd3bc468cececf3
Tavo <tsoto111@gmail.com>
2020-02-27 23:34:25 -0600

    Optimizing column widgets to use widget buildKey with params

--- lib/main.dart
+++ lib/main.dart
@@ -4,76 +4,40 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  void playSound(filenameIndex) {
+  void playSound(int filenameIndex) {
     final player = AudioCache();
     player.play("note$filenameIndex.wav");
+  }
+
+  Widget buildKey({MaterialColor keyColor, int soundIndex}) {
+    return Expanded(
+      child: RawMaterialButton(
+        fillColor: keyColor,
+        onPressed: () {
+          playSound(soundIndex);
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              child: RawMaterialButton(
-                fillColor: Colors.red,
-                onPressed: () {
-                  playSound(1);
-                },
-              ),
-            ),
-            Expanded(
-              child: RawMaterialButton(
-                fillColor: Colors.orange,
-                onPressed: () {
-                  playSound(2);
-                },
-              ),
-            ),
-            Expanded(
-              child: RawMaterialButton(
-                fillColor: Colors.yellow,
-                onPressed: () {
-                  playSound(3);
-                },
-              ),
-            ),
-            Expanded(
-              child: RawMaterialButton(
-                fillColor: Colors.green,
-                onPressed: () {
-                  playSound(4);
-                },
-              ),
-            ),
-            Expanded(
-              child: RawMaterialButton(
-                fillColor: Colors.green.shade900,
-                onPressed: () {
-                  playSound(5);
-                },
-              ),
-            ),
-            Expanded(
-              child: RawMaterialButton(
-                fillColor: Colors.blue,
-                onPressed: () {
-                  playSound(6);
-                },
-              ),
-            ),
-            Expanded(
-              child: RawMaterialButton(
-                fillColor: Colors.purple,
-                onPressed: () {
-                  playSound(7);
-                },
-              ),
-            ),
-          ],
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(keyColor: Colors.red, soundIndex: 1),
+              buildKey(keyColor: Colors.orange, soundIndex: 2),
+              buildKey(keyColor: Colors.yellow, soundIndex: 3),
+              buildKey(keyColor: Colors.lightGreen, soundIndex: 4),
+              buildKey(keyColor: Colors.green, soundIndex: 5),
+              buildKey(keyColor: Colors.blue, soundIndex: 6),
+              buildKey(keyColor: Colors.purple, soundIndex: 7),
+            ],
+          ),
         ),
       ),
     );
